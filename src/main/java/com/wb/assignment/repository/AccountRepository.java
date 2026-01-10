c0f99b29d1be0641181c43147d55bc5b8ad15a82
LENOVO <tahakasimsaifee53@gmail.com>
2026-01-10 21:18:15 +0300

    removed unwanted imports

--- src/main/java/com/wb/assignment/repository/AccountRepository.java
+++ src/main/java/com/wb/assignment/repository/AccountRepository.java
@@ -4,8 +4,6 @@ import com.wb.assignment.model.entity.Account;
 import com.wb.assignment.model.enums.AccountType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface AccountRepository extends JpaRepository<Account, String> {
 
     long countByCustomerId(String customerId);
